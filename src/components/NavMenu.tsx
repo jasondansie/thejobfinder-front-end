63c7466933c7e16984d75249a33e41ee302d3fd5
jasondansie <jasondansie@gmail.com>
2023-06-07 15:06:27 +0300

    adds a responsive menu for mobile

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const NavMenu = () => {
-    return (
-        <nav>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand>
-                        <LinkContainer to="/home">
-                            <Nav.Link>TheJobFinder</Nav.Link>
-                        </LinkContainer>
-                    </Navbar.Brand>
-                    <Nav className="me-auto">                 
-                        <LinkContainer to="/jobList">
-                            <Nav.Link>Jobs</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/addjob">
-                            <Nav.Link>Add a Job</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/resume">
-                            <Nav.Link>Resume</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/interviewTips">
-                            <Nav.Link>Interview tips</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Container>
-            </Navbar>
-            <br />
-        </nav>
-    ); 
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <nav>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand>
+            <LinkContainer to="/home">
+              <Nav.Link>TheJobFinder</Nav.Link>
+            </LinkContainer>
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            onClick={handleToggle}
+          />
+          <Navbar.Collapse id="responsive-navbar-nav" in={expanded}>
+            <Nav className="me-auto">
+              <LinkContainer to="/jobList">
+                <Nav.Link>Jobs</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/addjob">
+                <Nav.Link>Add a Job</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/resume">
+                <Nav.Link>Resume</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/interviewTips">
+                <Nav.Link>Interview tips</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <br />
+    </nav>
+  );
 };
 
 export default NavMenu;
