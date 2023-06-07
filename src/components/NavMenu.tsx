a88993bc4073e06b36d8957a800e834142c12fa9
jasondansie <jasondansie@gmail.com>
2023-06-07 14:55:50 +0300

    fixes routing to work on render.com

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const NavMenu = () => {
     return (
         <nav>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/home">TheJobFinder</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/jobList">List Jobs</Nav.Link>
-                        <Nav.Link href="/addjob">Add a Job</Nav.Link>
-                        <Nav.Link href="/resume">Resume</Nav.Link>
-                        <Nav.Link href="/interviewTips">Interview tips</Nav.Link>
+                    <Navbar.Brand>
+                        <LinkContainer to="/home">
+                            <Nav.Link>TheJobFinder</Nav.Link>
+                        </LinkContainer>
+                    </Navbar.Brand>
+                    <Nav className="me-auto">                 
+                        <LinkContainer to="/jobList">
+                            <Nav.Link>Jobs</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/addjob">
+                            <Nav.Link>Add a Job</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/resume">
+                            <Nav.Link>Resume</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/interviewTips">
+                            <Nav.Link>Interview tips</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Container>
             </Navbar>
